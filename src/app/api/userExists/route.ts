060a7114227659275c54c80271a2342fbe530961
Ifeanyi Ofikulu <ifeanyiofiks@gmail.com>
2024-10-13 19:42:17 +0100

    updated env

--- src/app/api/userExists/route.ts
+++ src/app/api/userExists/route.ts
@@ -2,15 +2,15 @@ import { NextResponse } from "next/server";
 import { connectMongoDB } from "../../../../lib/mongodb";
 import User from "../../../../models/user";
 
+export const dynamic = "force-dynamic";
 export async function POST(req: Request) {
-    try {
-        await connectMongoDB();
-        const {email} = await req.json();
-       const user = await User.findOne({email}).select('_id');
-        
-        return NextResponse.json({ user})
-    } catch (error) {
-        console.log(error);
-        
-    }
+  try {
+    await connectMongoDB();
+    const { email } = await req.json();
+    const user = await User.findOne({ email }).select("_id");
+
+    return NextResponse.json({ user });
+  } catch (error) {
+    console.log(error);
+  }
 }
--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,13 +2,11 @@ import { NextResponse } from "next/server";
 import { connectMongoDB } from "../../../../lib/mongodb";
 import User from "../../../../models/user";
 
-
+export const dynamic = "force-dynamic";
 export async function GET() {
-    
-      
-      await connectMongoDB();
-      const users = await User.find({}, 'name email'); // Fetch only username and email
-      // const users = await User.findOneAndUpdate({email:'john3@gmail.com'}, {passkeydone:false}); 
+  await connectMongoDB();
+  const users = await User.find({}, "name email"); // Fetch only username and email
+  // const users = await User.findOneAndUpdate({email:'john3@gmail.com'}, {passkeydone:false});
 
   return NextResponse.json(users);
 }
